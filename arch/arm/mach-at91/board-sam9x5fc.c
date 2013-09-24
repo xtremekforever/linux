dbb7d41c1a307b2d779710c487e794a05b1bf5bf
Jesse L. Zamora <xtremekforever@gmail.com>
2013-09-23 20:43:46 -0400

    Remove RevA definitions

--- arch/arm/mach-at91/board-sam9x5fc.c
+++ arch/arm/mach-at91/board-sam9x5fc.c
@@ -114,96 +114,6 @@ static struct mci_platform_data __initdata mci1_data = {
 };
 
 /*
- *  ISI
- */
-#if defined(CONFIG_VIDEO_ATMEL_ISI) || defined(CONFIG_VIDEO_ATMEL_ISI_MODULE)
-static struct isi_platform_data __initdata isi_data = {
-	.frate		= ISI_CFG1_FRATE_CAPTURE_ALL,
-	.has_emb_sync	= 0,
-	.emb_crc_sync = 0,
-	.hsync_act_low = 0,
-	.vsync_act_low = 0,
-	.pclk_act_falling = 0,
-	/* to use codec and preview path simultaneously */
-	.isi_full_mode = 1,
-	.data_width_flags = ISI_DATAWIDTH_8 | ISI_DATAWIDTH_10,
-};
-
-static void __init isi_set_clk(void)
-{
-	struct clk *pck0;
-	struct clk *plla;
-
-	pck0 = clk_get(NULL, "pck0");
-	plla = clk_get(NULL, "plla");
-
-	clk_set_parent(pck0, plla);
-	/* for the sensor ov9655: 10< Fclk < 48, Fclk typ = 24MHz */
-	clk_set_rate(pck0, 25000000);
-	clk_enable(pck0);
-}
-#else
-static void __init isi_set_clk(void) {}
-static struct isi_platform_data __initdata isi_data;
-#endif
-
-/*
- * soc-camera OV2640
- */
-#if defined(CONFIG_SOC_CAMERA_OV2640)
-static unsigned long isi_camera_query_bus_param(struct soc_camera_link *link)
-{
-	/* ISI board for ek using default 8-bits connection */
-	return SOCAM_DATAWIDTH_8;
-}
-
-static int i2c_camera_power(struct device *dev, int on)
-{
-	/* enable or disable the camera */
-	pr_debug("%s: %s the camera\n", __func__, on ? "ENABLE" : "DISABLE");
-	at91_set_gpio_output(AT91_PIN_PA13, on ? 0 : 1);
-
-	if (!on)
-		goto out;
-
-	/* If enabled, give a reset impulse */
-	at91_set_gpio_output(AT91_PIN_PA7, 0);
-	msleep(20);
-	at91_set_gpio_output(AT91_PIN_PA7, 1);
-	msleep(100);
-
-out:
-	return 0;
-}
-
-static struct i2c_board_info i2c_camera = {
-	I2C_BOARD_INFO("ov2640", 0x30),
-};
-
-static struct soc_camera_link iclink_ov2640 = {
-	.bus_id		= 0,
-	.board_info	= &i2c_camera,
-	.i2c_adapter_id	= 0,
-	.power		= i2c_camera_power,
-	.query_bus_param	= isi_camera_query_bus_param,
-};
-
-static struct platform_device isi_ov2640 = {
-	.name	= "soc-camera-pdrv",
-	.id	= 0,
-	.dev	= {
-		.platform_data = &iclink_ov2640,
-	},
-};
-
-static struct platform_device *soc_camera_devices[] __initdata = {
-	&isi_ov2640,
-};
-#else
-static struct platform_device *soc_camera_devices[] __initdata = {};
-#endif
-
-/*
  * LCD Controller
  */
 #if defined(CONFIG_FB_ATMEL) || defined(CONFIG_FB_ATMEL_MODULE)
@@ -283,55 +193,35 @@ static struct i2c_board_info __initdata ek_i2c_devices[] = {
 #endif
 #if defined(CONFIG_KEYBOARD_TCA8418)
   {
-		I2C_BOARD_INFO("tca8418_keypad", 0x34),
+		I2C_BOARD_INFO("tca8418_keypad", 0x34)
   },
 #endif
 };
 
 static void __init ek_board_configure_pins(void)
 {
-	if (ek_is_revA()) {
-		/* Port A is shared with gadget port */
-		/*ek_usbh_fs_data.vbus_pin[0] = AT91_PIN_PD9;*/
-		/*ek_usbh_hs_data.vbus_pin[0] = AT91_PIN_PD9;*/
-		ek_usbh_fs_data.vbus_pin[1] = AT91_PIN_PD10;
-		ek_usbh_hs_data.vbus_pin[1] = AT91_PIN_PD10;
-		/* Port C is full-speed only */
-		ek_usbh_fs_data.vbus_pin[2] = AT91_PIN_PD11;
+  /* Port A is shared with gadget port */
+	ek_usbh_fs_data.vbus_pin[1] = AT91_PIN_PD19;
+	ek_usbh_hs_data.vbus_pin[1] = AT91_PIN_PD19;
+	/* Port C is full-speed only */
+	ek_usbh_fs_data.vbus_pin[2] = AT91_PIN_PD20;
 
-		ek_usba_udc_data.vbus_pin = AT91_PIN_PB8;
+	ek_usba_udc_data.vbus_pin = AT91_PIN_PB16;
 
-		ek_macb0_data.phy_irq_pin = 0;
+	ek_macb0_data.phy_irq_pin = AT91_PIN_PB8;
 
-		mci0_data.slot[0].detect_pin = AT91_PIN_PD13;
-	} else {
-		/* Port A is shared with gadget port */
-		/*ek_usbh_fs_data.vbus_pin[0] = AT91_PIN_PD18;*/
-		/*ek_usbh_hs_data.vbus_pin[0] = AT91_PIN_PD18;*/
-		ek_usbh_fs_data.vbus_pin[1] = AT91_PIN_PD19;
-		ek_usbh_hs_data.vbus_pin[1] = AT91_PIN_PD19;
-		/* Port C is full-speed only */
-		ek_usbh_fs_data.vbus_pin[2] = AT91_PIN_PD20;
-
-		ek_usba_udc_data.vbus_pin = AT91_PIN_PB16;
-
-		ek_macb0_data.phy_irq_pin = AT91_PIN_PB8;
-
-		mci0_data.slot[0].detect_pin = AT91_PIN_PD15;
+	mci0_data.slot[0].detect_pin = AT91_PIN_PD15;
 
 #if defined(CONFIG_KEYBOARD_QT1070)
-		if (!cpu_is_at91sam9g25())
-			/* conflict with ISI */
-			at91_set_gpio_input(ek_i2c_devices[1].irq, 1);
+	if (!cpu_is_at91sam9g25())
+		/* conflict with ISI */
+		at91_set_gpio_input(ek_i2c_devices[1].irq, 1);
 #endif
-	}
 }
 
 static void __init ek_board_init(void)
 {
 	u32 cm_config;
-	int i;
-	bool config_isi_enabled = false;
 
 	cm_board_init(&cm_config);
 	ek_board_configure_pins();
@@ -348,33 +238,15 @@ static void __init ek_board_init(void)
 	/* MMC0 */
 	at91_add_device_mci(0, &mci0_data);
 	/* I2C */
-	if (cm_config & CM_CONFIG_I2C0_ENABLE)
+	//if (cm_config & CM_CONFIG_I2C0_ENABLE)
 		i2c_register_board_info(0,
 				ek_i2c_devices, ARRAY_SIZE(ek_i2c_devices));
-	else
+	/*else
 		at91_add_device_i2c(0,
 				ek_i2c_devices, ARRAY_SIZE(ek_i2c_devices));
+  */
 
-	if (cpu_is_at91sam9g25()) {
-		/* ISI */
-		/* NOTE: PCK0 provides ISI_MCK to the ISI module.
-		 * ISI's PWD pin (PA13) conflicts with MCI1_CK, and SPI0_SPCK
-		 * due to hardware design.
-		 * Do not add ISI device if SPI0 is enabled.
-		 */
-		 if (!(cm_config & CM_CONFIG_SPI0_ENABLE)) {
-			platform_add_devices(soc_camera_devices,
-					ARRAY_SIZE(soc_camera_devices));
-			isi_set_clk();
-			at91_add_device_isi(&isi_data);
-			for (i = 0; i < ARRAY_SIZE(soc_camera_devices); i++) {
-				if (soc_camera_devices[i]->name != NULL) {
-					config_isi_enabled = true;
-					break;
-				}
-			}
-		 }
-	} else if (!cpu_is_at91sam9x25()) {
+	if (!cpu_is_at91sam9x25()) {
 		/* LCD Controller */
 		at91_add_device_lcdc(&ek_lcdc_data);
 		/* Touch Screen */
@@ -385,7 +257,7 @@ static void __init ek_board_init(void)
 	/* Conflict between SPI0, MCI1 and ISI pins.
 	 * add MCI1 only if SPI0 and ISI are both disabled.
 	 */
-	if (!(cm_config & CM_CONFIG_SPI0_ENABLE) && !config_isi_enabled)
+	if (!(cm_config & CM_CONFIG_SPI0_ENABLE))
 		at91_add_device_mci(1, &mci1_data);
 
 #if 0
@@ -409,18 +281,10 @@ static void __init ek_board_init(void)
 	/* SMD */
 	at91_add_device_smd();
 
-	if (ek_is_revA())
-		printk(KERN_CRIT "AT91: EK rev A\n");
-	else
-		printk(KERN_CRIT "AT91: EK rev B and higher\n");
-
 	/* print conflict information */
 	if (cm_config & CM_CONFIG_SPI0_ENABLE)
 		printk(KERN_CRIT
 			"AT91: SPI0 conflicts with MCI1 and ISI, disable MCI1 and ISI\n");
-	else if (config_isi_enabled)
-		printk(KERN_CRIT
-			"AT91: ISI conficts with MCI1, disable MCI1\n");
 }
 
 MACHINE_START(AT91SAM9X5FC, "Ahern AT91SAM9X5 Fuel Controller")
